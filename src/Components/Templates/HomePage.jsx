90b398648010be92ce518ada7fb0fc90c2b92d9e
Soudabeh Noorollahi <s.nourollahii@gmail.com>
2024-12-05 10:18:59 +0100

    Added currency selection functionality with support for dynamic updates

--- src/Components/Templates/HomePage.jsx
+++ src/Components/Templates/HomePage.jsx
@@ -3,25 +3,28 @@ import { useEffect } from "react";
 import TableCoin from "../Modules/TableCoin";
 import { getCoinList } from "../../Services/cryptoApi";
 import Pagination from "../Modules/Pagination";
+import Search from "../Modules/Search";
 
 function HomePage() {
   const [coins, setCoins] = useState([]);
   const [isLoading, setIsLoading] = useState(true);
   const [page, setPage] = useState(1);
+  const [currency, setCurrency] = useState("usd");
 
   useEffect(() => {
     setIsLoading(true);
     const getData = async () => {
-      const res = await fetch(getCoinList(page));
+      const res = await fetch(getCoinList(page, currency));
       const json = await res.json();
       setCoins(json);
       setIsLoading(false);
     };
     getData();
-  }, [page]);
+  }, [page, currency]);
 
   return (
     <div>
+      <Search currency={currency} setCurrency={setCurrency} />
       <TableCoin coins={coins} isLoading={isLoading} />
       <Pagination page={page} setPage={setPage} />
     </div>
